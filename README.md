babec844b36d62afd1d1c98a6f4f7c127cc51527
PonnyTheLight <davidramossaiz2004@gmail.com>
2024-12-10 11:04:54 +0100

    Release 1.0.0

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+VERSION 0.1.1
+
+* Added rounded border for the navbar image.
+* Added scale animations for buttons, info and team.
+* 
+
 VERSION 0.1.0
 
 * Started the project with the base created before.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,8 @@ export default function Home() {
           <p className="text-xl text-gray-400">Sumergete en una aventura unica por valladolid como Medico, Bombero, Policia entre otos equipos de emergencias y mantenimiento de la ciudad.</p>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 justify-center mt-6">
-            <Link className="text-xl w-full bg-blue-700 border-2 border-blue-200 text-white p-2 px-4 rounded-xl mr-2" target="_blank" href={'https://www.roblox.com/games/18377062010/ALPHA-Emergency-Valladolid'} >Jugar EV</Link>
-            <Link className="text-xl bg-transparent w-full border-2 border-inherit border-white text-white p-2 px-4 rounded-xl" target="_blank" href={'https://www.youtube.com/watch?v=BdkQACMQZPo&ab_channel=Vairos'} >Trailer</Link>
+            <Link className="hover:scale-90 text-xl w-full bg-blue-700 border-2 border-blue-200 text-white p-2 px-4 rounded-xl mr-2" target="_blank" href={'https://www.roblox.com/games/18377062010/ALPHA-Emergency-Valladolid'} >Jugar EV</Link>
+            <Link className="hover:scale-90 text-xl bg-transparent w-full border-2 border-inherit border-white text-white p-2 px-4 rounded-xl" target="_blank" href={'https://www.youtube.com/watch?v=BdkQACMQZPo&ab_channel=Vairos'} >Trailer</Link>
           </div>
         </div>
       </section>
@@ -27,16 +27,53 @@ export default function Home() {
       <GameInfoComp />
 
       <section className="text-center w-[90%] justify-center my-12 justify-self-center">
-        <h1 className="text-2xl mb-2">¡ESTE ES NUESTRO EQUIPO!</h1>
+        <h1 className="text-2xl mb-3">¡ESTE ES NUESTRO EQUIPO ACTUAL!</h1>
         <div className="flex gap-12 flex-wrap justify-center">
-          <div>
-            <Image className="rounded-full bg-blue-500" src={'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-2250B80B5CD4565BD2937127248E099D-Png/150/150/AvatarHeadshot/Webp/noFilter'} width={100} height={100} alt="UserProfile_zLowyG"></Image>
+          <div className="hover:scale-110">
+            <Image className="rounded-full justify-self-center bg-blue-500" src={'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-2250B80B5CD4565BD2937127248E099D-Png/150/150/AvatarHeadshot/Webp/noFilter'} width={100} height={100} alt="UserProfile_VairosOficial"></Image>
             <Link href={'https://www.roblox.com/users/1680662240/profile'} className="text-blue-600">VairosOficial</Link>
             <p className="text-gray-700">Game Founder</p>
           </div>
+          <div className="hover:scale-110">
+            <Image className="rounded-full justify-self-center bg-blue-500" src={'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-B7432CDEBEF967A1836BD5EE957FC998-Png/150/150/AvatarHeadshot/Webp/noFilter'} width={100} height={100} alt="UserProfile_eloy098"></Image>
+            <Link href={'https://www.roblox.com/users/95941362/profile'} className="text-blue-600">eloy098</Link>
+            <p className="text-gray-700">Game Co-Owner</p>
+          </div>
 
-          <div>
-            <Image className="rounded-full bg-blue-500" src={'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-1F90BA5025883AC55EF796736F31CC80-Png/150/150/AvatarHeadshot/Webp/noFilter'} width={100} height={100} alt="UserProfile_zLowyG"></Image>
+          <div className="hover:scale-110">
+            <Image className="rounded-full justify-self-center bg-blue-500" src={'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-CAAA9821CC76D8FFD7A63EE3BCB3D1F5-Png/150/150/AvatarHeadshot/Webp/noFilter'} width={100} height={100} alt="UserProfile_juantxete"></Image>
+            <Link href={'https://www.roblox.com/users/590120827/profile'} className="text-blue-600">juantxete</Link>
+            <p className="text-gray-700">--</p>
+          </div>
+          <div className="hover:scale-110">
+            <Image className="rounded-full justify-self-center bg-blue-500" src={'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-65EDF08422F1BE2D4C367D0816E1DC8A-Png/150/150/AvatarHeadshot/Webp/noFilter'} width={100} height={100} alt="UserProfile_TobalMJ"></Image>
+            <Link href={'https://www.roblox.com/users/985861937/profile'} className="text-blue-600">TobalMJ</Link>
+            <p className="text-gray-700">Game Developer</p>
+          </div>
+          <div className="hover:scale-110">
+            <Image className="rounded-full justify-self-center bg-blue-500" src={'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-D42E6323009C812519EF9BC319BDFD4A-Png/150/150/AvatarHeadshot/Webp/noFilter'} width={100} height={100} alt="UserProfile_MRGAMER_03"></Image>
+            <Link href={'https://www.roblox.com/users/589175438/profile'} className="text-blue-600">MRGAMER_03</Link>
+            <p className="text-gray-700">Builder</p>
+          </div>
+          <div className="hover:scale-110">
+            <Image className="rounded-full justify-self-center bg-blue-500" src={'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-D42E6323009C812519EF9BC319BDFD4A-Png/150/150/AvatarHeadshot/Webp/noFilter'} width={100} height={100} alt="UserProfile_MRGAMER_03"></Image>
+            <Link href={'https://www.roblox.com/users/--/profile'} className="text-blue-600">Magik</Link>
+            <p className="text-gray-700">--</p>
+          </div>
+          <div className="hover:scale-110">
+            <Image className="rounded-full justify-self-center bg-blue-500" src={'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-D42E6323009C812519EF9BC319BDFD4A-Png/150/150/AvatarHeadshot/Webp/noFilter'} width={100} height={100} alt="UserProfile_MRGAMER_03"></Image>
+            <Link href={'https://www.roblox.com/users/--/profile'} className="text-blue-600">p4tkaro</Link>
+            <p className="text-gray-700">--</p>
+          </div>
+
+          <div className="hover:scale-110">
+            <Image className="rounded-full justify-self-center bg-blue-500" src={'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-F64D8BCDAC96CC1BCA0E0626E1A0C18E-Png/150/150/AvatarHeadshot/Webp/noFilter'} width={100} height={100} alt="UserProfile_AlexProOfficial"></Image>
+            <Link href={'https://www.roblox.com/users/3392027959/profile'} className="text-blue-600">AlexProOficial</Link>
+            <p className="text-gray-700">Game Developer</p>
+          </div>
+
+          <div className="hover:scale-110">
+            <Image className="rounded-full justify-self-center bg-blue-500" src={'https://tr.rbxcdn.com/30DAY-AvatarHeadshot-1F90BA5025883AC55EF796736F31CC80-Png/150/150/AvatarHeadshot/Webp/noFilter'} width={100} height={100} alt="UserProfile_zLowyG"></Image>
             <Link href={'https://www.roblox.com/users/2634949286/profile'} className="text-blue-600">zLowyG</Link>
             <p className="text-gray-700">Web Developer</p>
           </div>
